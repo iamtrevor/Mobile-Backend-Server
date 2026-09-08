8734f74d655a92a58a11c59fe4099595178ed6e3
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-08 11:13:23 +0300

    first commit

---
+++ src/main/kotlin/Monitoring.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.request.path
+import org.slf4j.event.Level
+
+
+fun Application.configureMonitoring() {
+    install(CallLogging)
+}
