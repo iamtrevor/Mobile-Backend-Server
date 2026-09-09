5611869165dd87c092329d51a993d164f7ecf3dd
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-09 05:15:48 +0300

    creating searchHeroes endpoint

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,6 +2,7 @@ package com.example
 
 import com.example.routes.getAllHeroes
 import com.example.routes.root
+import com.example.routes.searchHeroes
 import io.ktor.server.application.*
 import io.ktor.server.http.content.resources
 import io.ktor.server.http.content.static
@@ -14,6 +15,7 @@ fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+        searchHeroes()
 
         static("/images"){
             resources("images")
--- src/main/kotlin/repository/HeroRepository.kt
+++ src/main/kotlin/repository/HeroRepository.kt
@@ -16,6 +16,6 @@ interface HeroRepository {
     val page5 : List<Hero>
 
     suspend fun getAllHeroes(page : Int = 1): ApiResponse
-    suspend fun searchHeroes(name : String) : ApiResponse
+    suspend fun searchHeroes(name : String?) : ApiResponse
 
 }
