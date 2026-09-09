c0df7e231da7c9fc9dbafaf3f7faf0a3836b4449
iamtrevor <trevor.k.gachagua@gmail.com>
2026-09-09 05:16:50 +0300

    creating searchHeroes endpoint

--- src/main/kotlin/routes/SearchHeroes.kt
+++ src/main/kotlin/routes/SearchHeroes.kt
@@ -2,6 +2,7 @@ package com.example.routes
 
 import com.example.repository.HeroRepository
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import org.koin.ktor.ext.inject
